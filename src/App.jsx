4e24167cd1dbbcae78d4940c449801f49473d06b
Deepanshu Mishra <d4deepanshu723@gmail.com>
2024-04-02 20:32:48 +0530

    resp

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import viteLogo from '/vite.svg'
 import './index.css'
 import Notification from './components/Notification'
 import Sidebar from './components/Sidebar'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
+  const [bar,setBar] = useState(false);
   return (
     <main>
       <Notification/>
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const Notification = () => {
   return (
-    <div className="bg-blue-500 h-10 w-full flex items-center justify-between font-roboto">
+    <div className="bg-blue-500 md:h-10 lg:h-10 h-20 w-full flex items-center justify-between font-roboto">
       <h1 className="text-center text-white text-sm flex-grow">Enable browser notifications to avoid missing out on important activity.</h1>
       <div>
         <button className="text-xs text-white font-medium rounded p-1 text-center inline-flex items-center bg-transparent border border-gray-300 border-opacity-100 backdrop-blur-4xl hover:bg-white hover:bg-opacity-20 focus:outline-none focus:bg-white focus:bg-opacity-20 mr-2">
@@ -12,7 +12,7 @@ const Notification = () => {
           Enable Notifications
         </button>
 
-        <FontAwesomeIcon icon={faX} className='text-white font-extralight mr-2 cursor-pointer'/>
+        <FontAwesomeIcon icon={faX} className='text-white font-extralight hidden md:inline  mr-2 cursor-pointer'/>
       </div>
     </div>
   );
